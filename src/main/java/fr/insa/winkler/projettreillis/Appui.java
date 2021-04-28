2f44e44c2c5a19e9de0f4531654bd9e306d555a3
marie-projet marie.winkler@insa-strasbourg.fr <mariewinkler@air-de-marie>
2021-04-28 18:16:21 +0200

    implémentation de la méthode getPos()

--- src/main/java/fr/insa/winkler/projettreillis/Appui.java
+++ src/main/java/fr/insa/winkler/projettreillis/Appui.java
@@ -5,6 +5,7 @@
  */
 package fr.insa.winkler.projettreillis;
 
+
 /**
  *
  * @author mariewinkler
@@ -23,6 +24,33 @@ public abstract class Appui extends Noeud{
         this.point1=point1;
         this.point2=point2;
         this.position=position;
+    }
+    
+    public Point getPos(){
+        Point p1=new Point(0,0);
+        Point p2=new Point(0,0);
+        if(this.point1==0){
+            p1=this.getTerrain().getPT0();
+        }
+        if(this.point1==1){
+            p1=this.getTerrain().getPT1();
+        }
+        if(this.point1==2){
+            p1=this.getTerrain().getPT2();
+        }
+        if(this.point2==0){
+            p2=this.getTerrain().getPT0();
+        }
+        if(this.point2==1){
+            p2=this.getTerrain().getPT1();
+        }
+        if(this.point2==2){
+            p2=this.getTerrain().getPT2();
+        }
+        double x=(Math.max(p1.getX(), p2.getX())-Math.min(p1.getX(), p2.getX()))*this.getPosition()+Math.min(p1.getX(), p2.getX());
+        double y=(Math.max(p1.getY(), p2.getY())-Math.min(p1.getY(),p2.getY()))*this.getPosition()+Math.min(p1.getY(), p2.getY());
+        Point p=new Point(x,y);
+        return p;
     }
 
     public TriangleTerrain getTerrain() {
